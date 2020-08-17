1db026a410d08c3fb72d7d3dc7105fbfbf0cca3f
windstep <icq431@yandex.ru>
2020-08-17 16:55:47 +0300

    Добавил логику на backend для обработки поиска по пользователям и заданиям

--- frontend/controllers/TasksController.php
+++ frontend/controllers/TasksController.php
@@ -2,7 +2,9 @@
 
 namespace frontend\controllers;
 
+use frontend\models\Category;
 use frontend\models\Task;
+use Yii;
 use yii\web\Controller;
 
 class TasksController extends Controller
@@ -14,8 +16,31 @@ class TasksController extends Controller
             ->where(['status' => $status])
             ->joinWith('category')
             ->joinWith('city')
-            ->orderBy('created_at DESC')
-            ->all();
-        return $this->render('browse', ['tasks' => $tasks]);
+            ->orderBy('created_at DESC');
+
+        if (Yii::$app->request->get('remote')) {
+            $tasks->where('address is null');
+        }
+
+        if (Yii::$app->request->get('no_applications')) {
+            $tasks->leftJoin('applications', 'applications.task_id = id')
+                ->having('count(applications.id) > 0')
+                ->groupBy('tasks.id');
+        }
+
+        $name = Yii::$app->request->get('name');
+        if ($name && $name !== '') {
+            $tasks->where(['like', 'title', "%$name%"]);
+        }
+
+        if (Yii::$app->request->get('period')) {
+
+        }
+
+        $tasks = $tasks->all();
+
+        $categories = Category::find()->all();
+
+        return $this->render('browse', ['tasks' => $tasks, 'categories' => $categories]);
     }
 }
--- frontend/controllers/UsersController.php
+++ frontend/controllers/UsersController.php
@@ -2,8 +2,10 @@
 
 namespace frontend\controllers;
 
+use DateTime;
 use frontend\models\User;
 use frontend\models\UsersQuery;
+use Yii;
 use yii\db\Query;
 use yii\web\Controller;
 
@@ -25,8 +27,39 @@ class UsersController extends Controller
             ->leftJoin('feedback', 'feedback.user_id = users.id')
             ->with('categories')
             ->orderBy('created_at desc')
-            ->groupBy('users.id')
-            ->all();
-        return $this->render('browse', ['users' => $users]);
+            ->groupBy('users.id');
+
+
+        if (($categories = Yii::$app->request->get('categories')) && is_array($categories)) {
+            $users->leftJoin('category_user as cu', 'cu.user_id = users.id')
+                ->where(['in', 'cu.category_id', $categories]);
+        }
+
+        if (Yii::$app->request->get('online')) {
+            $lastVisit = (new \DateTimeImmutable())->modify('-5m')->format('Y-m-d H:i:s');
+            $users->where(['>=', 'last_visit', $lastVisit]);
+        }
+
+        if (Yii::$app->request->get('free')) {
+            $users->where('not exists ' .
+                '(select 1 from tasks where executor_id = users.id and status in ("wip", "new"))'
+            );
+        }
+
+        if (Yii::$app->request->get('bookmarked')) {
+            // здесь интересно. У нас пока нет авторизации, поэтому будем использовать хардкод id пользователя - 1
+            $users->where('exists ' .
+              '(select 1 from bookmarks where bookmark_user_id = users.id and bookmarks.user_id = 1)'
+            );
+        }
+
+        if (Yii::$app->request->get('hasFeedback')) {
+            $users->leftJoin('feedbacks', 'user_id = users.id')
+                ->groupBy('users.id')
+                ->having('count(feedback.id) > 0');
+        }
+
+
+        return $this->render('browse', ['users' => $users->all()]);
     }
 }
