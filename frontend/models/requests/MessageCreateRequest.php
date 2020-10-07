831b7dd5d33f724d857a28bed6a2ed73be586992
windstep <icq431@yandex.ru>
2020-10-07 09:46:56 +0300

    Заменил на beforevalidate

--- frontend/models/requests/MessageCreateRequest.php
+++ frontend/models/requests/MessageCreateRequest.php
@@ -8,43 +8,28 @@ use Yii;
 use yii\base\Model;
 use yii\db\ActiveRecord;
 
-class MessageCreateRequest extends ActiveRecord
+class MessageCreateRequest extends Message
 {
     public $task_id;
     public $message;
 
-    public static function tableName()
+    public function beforeValidate()
     {
-        return 'messages';
-    }
-
-    public function rules()
-    {
-        return [
-            [['message', 'task_id'], 'required'],
-            [['message', 'task_id'], 'safe'],
-            ['message', 'string', 'min' => 1],
-            ['task_id', 'exist', 'targetClass' => Task::class, 'targetAttribute' => 'id'],
-        ];
-    }
-
-    public function save($runValidation = true, $attributeNames = null)
-    {
-        if (!$this->validate()) {
-            return false;
-        }
-
         /** @var Task $task */
         $task = Task::findOne(['id' => $this->task_id]);
-        $userId = Yii::$app->user->getId() ?? 21;
+        $userId = Yii::$app->user->getId();
 
-        $message = new Message();
-        $message->recipient_id = $task->author_id === $userId ? $task->author_id : $task->executor_id;
-        $message->author_id = $userId;
-        $message->task_id = $this->task_id;
-        $message->content = $this->message;
+        if (!$userId) {
+            throw new \InvalidArgumentException("User ID is not defined");
+        }
 
-        $message->save();
-        return true;
+        $this->message = Yii::$app->request->post('message');
+        $this->recipient_id = $task->author_id === $userId ? $task->author_id : $task->executor_id;
+        $this->author_id = $userId;
+        $this->content = Yii::$app->request->post('message');
+        $this->task_id = Yii::$app->request->post('task_id');
+
+        var_dump($this);
+        die();
     }
 }
